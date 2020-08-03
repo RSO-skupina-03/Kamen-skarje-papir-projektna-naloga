1d2815a7ff1d347c8b8aae84e12d5c729b85e901
bernardkucina <kucina.berni@gmail.com>
2020-08-03 17:28:25 +0200

    V model.py dodani funkciji nova_igra in nova_igra_1 in lepši tekstovni umesnik

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from random import randint
 
-MOZNOSTI = ['Kamen', 'Skarje', 'Papir']
-MOZNOSTI_2 = ['Kamen', 'Skarje', 'Papir', 'Voda', 'Ogenj']
+MOZNOSTI = ['Kamen', 'Škarje', 'Papir']
+MOZNOSTI_2 = ['Kamen', 'Škarje', 'Papir', 'Voda', 'Ogenj']
 
 class Igra:
 
@@ -25,7 +25,7 @@ class KamenSkarjePapir(Igra):
 
     def potek_igre(self, izbrano_orozje):
     # Sprememba!!! igra se do 7 iger in se bo potem določilo zmagovalca
-            slovar_izbir = {'Kamen': 0, 'Skarje': 1, 'Papir': 2}
+            slovar_izbir = {'Kamen': 0, 'Škarje': 1, 'Papir': 2}
 
             igralec = slovar_izbir.get(MOZNOSTI[izbrano_orozje])
             racunalnik = slovar_izbir.get(self.izberi_orozje_racunalnik())
@@ -44,6 +44,7 @@ class KamenSkarjePapir(Igra):
                 self.tocka_za_racunalnik()
             else:
                 pass
+
     def izberi_orozje_racunalnik(self):
         return MOZNOSTI[randint(0, 2)]
 
@@ -52,9 +53,6 @@ class KamenSkarjePapir(Igra):
     
     def zmaga_igralca(self):
         return self.igralec > self.racunalnik and self.konec_igre() == True
-
-    def poraz_igralca(self):
-        return self.igralec < self.racunalnik and self.konec_igre() == True
 
     def koncni_izid_racunalnika(self):
         if self.konec_igre() == True:
@@ -72,12 +70,10 @@ class KamenSkarjePapirOgenjVoda(Igra):
 
     def potek_igre_1(self, izbrano_orozje):
 
-        while self.konec_igre_1() == False:
-            slovar_izbir = {'Kamen': 0, 'Skarje': 1, 'Papir': 2, 'Ogenj': 3, 'Voda': 4}
+            slovar_izbir = {'Kamen': 0, 'Škarje': 1, 'Papir': 2, 'Ogenj': 3, 'Voda': 4}
 
             igralec = slovar_izbir.get(MOZNOSTI_2[izbrano_orozje])
             racunalnik = slovar_izbir.get(self.izberi_orozje_1_racunalnik())
-
 
             mozni_izidi = [
                 [0, 1, -1, -1, 1],
@@ -102,9 +98,6 @@ class KamenSkarjePapirOgenjVoda(Igra):
     def zmaga_igralca_1(self):
         return self.igralec > self.racunalnik and self.konec_igre_1() == True
 
-    def poraz_igralca_1(self):
-        return self.igralec < self.racunalnik and self.konec_igre_1() == True
-
     def konec_igre_1(self):
         return self.racunalnik + self.igralec == 15
         #tukaj se bo igra igrala do 15 iger
@@ -119,6 +112,12 @@ class KamenSkarjePapirOgenjVoda(Igra):
             return self.igralec
         else:
             pass
+        
+def nova_igra():
+    return KamenSkarjePapir()
+
+def nova_igra_1():
+    return KamenSkarjePapirOgenjVoda
 
 
         
--- tekstovni_umesnik.py
+++ tekstovni_umesnik.py
@@ -1,34 +1,36 @@
-from model import KamenSkarjePapirOgenjVoda, KamenSkarjePapir
+import model
 
 # začetek tekstovnega umesnika da nekako vidim kako bom oblikoval program
-#def poraz(kamen_skarje_papir):
-    #return  f'IZGUBIL SI PROTI RAČUNALNIKU Z IZIDOM: 
-     #{self.koncni_izid_racunalnika()} : {self.koncni_izid_igralca()}'
+def poraz_ali_poraz(igra):
+    if igra.zmaga_igralca() == True:
+        return f'BRAVO, ZMAGAL SI MOGOČNI STROJ Z IZIDOM: {igra.koncni_izid_igralca()} : {igra.koncni_izid_racunalnika()}'
+    else:
+        return  f'IZGUBIL SI PROTI RAČUNALNIKU Z IZIDOM: {igra.koncni_izid_racunalnika()} : {igra.koncni_izid_igralca()}'
+def poraz_ali_poraz_1(igra):
+    if igra.zmaga_igralca_1 == True:
+        return f'BRAVO, ZMAGAL SI MOGOČNI STROJ Z IZIDOM: {igra.koncni_izid_igralca()} : {igra.koncni_izid_racunalnika()}'
+    else:
+        return f'IZGUBIL SI PROTI RAČUNALNIKU Z IZIDOM: {igra.koncni_izid_racunalnika()} : {igra.koncni_izid_igralca()}'
 
-#def zmaga(kamen_skarje_papir):
-    #return f'BRAVO, ZMAGAL SI MOGOČNI STROJ Z IZIDOM:
-     #{self.koncni_izid_igralca()} : {self.koncni_izid_racunalnika()}'
-
-#def delni_rezultat(kamen_skarje_papir):
-    #return f'TRENUTNI IZID:
-    #{self.delni_izid_igralca()} : {self.delni_izid_racunalnika()} '
+def delni_rezultat(igra):
+    return f'TRENUTNI IZID: {igra.delni_izid_igralca()} : {igra.delni_izid_racunalnika()}'
 
 def zacetni_menu():
     while True:
 
         print('''IZBERI SI IGRO:
-        1: Kamen škarje papir (igraš proti računalniku 7 iger)
-        2: Kamnen škraje papir ogenj voda (bolj atraktivna različica kjer se igra 14 iger)
+        1: Kamen škarje papir (igraš proti računalniku minimalno 7 iger)
+        2: Kamnen škraje papir ogenj voda (bolj atraktivna različica kjer se igra minimalno 14 iger)
         3: Zaključil sem z igranjem''')
 
         izbira = input('VPIŠITE IZBIRO: ')
 
         if izbira == '1':
-            ksp = KamenSkarjePapir()
+            ksp = model.nova_igra()
             print('DOBRODOŠLI V IGRI KAMEN ŠKARJE PAPIR')
             kamen_skarje_papir(ksp)
         elif izbira == '2':
-            kspov = KamenSkarjePapirOgenjVoda()
+            kspov = model.nova_igra_1()
             print('DOBRODOŠLI V IGRI KAMEN ŠKARJE PAPIR OGENJ')
             kamen_skarje_papir_ogenj_voda(kspov)
         elif izbira == '3':
@@ -47,14 +49,19 @@ def kamen_skarje_papir(ksp):
         ''')
 
     while ksp.konec_igre() == False:
+        print(delni_rezultat(ksp))
 
         izbira = input('> ')
+        racunalnik = ksp.izberi_orozje_racunalnik()
+        
+        print('RAČUNALNIK:', racunalnik)
         prava_izbira = int(izbira) - 1
 
         if prava_izbira == 0 or prava_izbira == 1 or prava_izbira == 2:
             ksp.potek_igre(prava_izbira)
         else:
             print('Vpišite številko pred orožjem')
+    print(poraz_ali_poraz(ksp))
 
 def kamen_skarje_papir_ogenj_voda(kspov):
     print('''
@@ -67,14 +74,19 @@ def kamen_skarje_papir_ogenj_voda(kspov):
     5: Voda''')
 
     while kspov.konec_igre_1() == False:
+        print(delni_rezultat(kspov))
 
         izbira = input('> ')
+        racunalnik = kspov.izberi_orozje_racunalnik()
+
+        print('RAČUNALNIK:', racunalnik)
         prava_izbira = int(izbira) - 1
 
         if prava_izbira == 0 or prava_izbira == 1 or prava_izbira == 2 or prava_izbira == 3 or prava_izbira == 4:
             kspov.potek_igre_1(prava_izbira)
         else:
             print('Vpišite ime orožja ali pa številko pred orožjem')
+    print(poraz_ali_poraz(kspov))
 
 zacetni_menu()
 
