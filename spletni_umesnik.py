df46de9ffe26384161bc2988f10596bd8b893d63
bernardkucina <kucina.berni@gmail.com>
2025-05-07 22:40:02 +0200

    http3 dela

--- spletni_umesnik.py
+++ spletni_umesnik.py
@@ -27,192 +27,305 @@ def error500(error):
 def server_static(filepath):
     return bottle.static_file(filepath, root='static')
 
-@bottle.get('/')
+@bottle.route('/', method=['GET','HEAD'])
 def zacetni_menu():
-    return bottle.template('views/log.tpl')
-
-@bottle.get('/end/')
-def zacetni_menu():
-    uporabnik = bottle.request.get_cookie("uporabnik", secret=STARI_SLOVENSKI_PREGOVOR)
-    if uporabnik is None:
-        return bottle.redirect(f"/")
+    if request.method == 'HEAD':
+        response.status = 200
+        return
     else:
-        return bottle.template('views/zacetni_menu.tpl', uporabnik=uporabnik.upper())
+        return bottle.template('views/log.tpl')
 
-@bottle.put("/zacetni_menu/")
+@bottle.route('/end/', method=['GET','HEAD'])
+def zacetni_menu():
+    if request.method == 'HEAD':
+        response.status = 200
+        return
+    else:
+        uporabnik = bottle.request.get_cookie("uporabnik", secret=STARI_SLOVENSKI_PREGOVOR)
+        if uporabnik is None:
+            response.status = 303
+            response.set_header("Location", "/")
+            return
+        else:
+            return bottle.template('views/zacetni_menu.tpl', uporabnik=uporabnik.upper())
+        
+@bottle.route('/zacetni_menu/', method=['OPTIONS'])
+def zacetni_menu_preflight():
+    response.set_header('Access-Control-Allow-Origin',  '*')
+    response.set_header('Access-Control-Allow-Methods', 'PUT, OPTIONS')
+    response.set_header('Access-Control-Allow-Headers', 'Content-Type')
+    return
+
+@bottle.route("/zacetni_menu/", method=["PUT","HEAD"])
 def prijava():
-    uporabnik = bottle.request.json.get("uporabnik")
-    password = bottle.request.json.get("password")
-    if uporabnik == "Gost" or uporabnik == "":
-        uporabnik = "Gost"
+    if request.method == "HEAD":
+        response.status = 303
+        response.set_header("Location", "/end/")
+        return
+    else:
+        uporabnik = bottle.request.json.get("uporabnik")
+        password = bottle.request.json.get("password")
+        if uporabnik == "Gost" or uporabnik == "":
+            uporabnik = "Gost"
     
-    ksp.nastavi_uporabnika(uporabnik)
-    kspov.nastavi_uporabnika(uporabnik)
+        bottle.response.set_cookie("uporabnik", uporabnik, path='/',secret=STARI_SLOVENSKI_PREGOVOR)
 
-    ksp.preberi_iz_datoteke()
-    kspov.preberi_iz_datoteke()
+        ksp.nastavi_uporabnika(uporabnik)
+        kspov.nastavi_uporabnika(uporabnik)
 
-    bottle.response.set_cookie("uporabnik", uporabnik, path='/',secret=STARI_SLOVENSKI_PREGOVOR)
-    return bottle.redirect(f"/end/")
+        ksp.preberi_iz_datoteke()
+        kspov.preberi_iz_datoteke()
+
+        response.status = 303
+        response.set_header("Location", "/end/")
+        return
 
 #================================================================================================================================================
 
-@bottle.get('/nova_igra_ksp/')
+@bottle.route('/nova_igra_ksp/', method=['GET','HEAD'])
 def nova_igra():
-    id_nova_igra = ksp.nova_igra()
-    # print(id_nova_igra)
-    bottle.response.set_cookie(ID_IGRE_COKOLADNI_PISKOT, str(id_nova_igra), path='/', secret=STARI_SLOVENSKI_PREGOVOR)
-    return bottle.redirect(f'/ksp/')
-
-@bottle.get('/ksp/')
-def igra_ksp():
-    id_igre = bottle.request.get_cookie(ID_IGRE_COKOLADNI_PISKOT, secret=STARI_SLOVENSKI_PREGOVOR)
-    if id_igre is None:
-        return bottle.redirect(f"/nova_igra_ksp/")
+    if request.method == 'HEAD':
+        response.status = 303
+        response.set_header("Location", "/ksp/")
+        return
     else:
-        id_igre = int(id_igre)
-        if id_igre not in ksp.igre:
-            return bottle.redirect(f"/nova_igra_ksp/")
-        
-        # print(id_igre)
-        igra = ksp.igre[id_igre]
-        return bottle.template("views/ksp.tpl", igra=igra, id_igre=id_igre)
-
-@bottle.post('/ksp/')
-def izbira_igralca_ksp():
-    id_igre = int(bottle.request.get_cookie(ID_IGRE_COKOLADNI_PISKOT, secret=STARI_SLOVENSKI_PREGOVOR))
-    orozje = int(bottle.request.forms["orozje"])
-    ksp.potek_igre(id_igre, orozje)
-    return bottle.redirect(f"/ksp/")
-
-@bottle.get("/zgodovina_ksp/")
-def prikazi_zgodovino():
-    uporabnik = bottle.request.get_cookie("uporabnik", secret=STARI_SLOVENSKI_PREGOVOR)
-    if uporabnik is None:
-        return bottle.redirect(f"/")
+        id_nova_igra = ksp.nova_igra()
+        # print(id_nova_igra)
+        bottle.response.set_cookie(ID_IGRE_COKOLADNI_PISKOT, str(id_nova_igra), path='/', secret=STARI_SLOVENSKI_PREGOVOR)
+        response.status = 303
+        response.set_header("Location", "/ksp/")
+        return
     
-    ksp.preberi_iz_datoteke()
-    igre_za_brisanje = [id_igre for id_igre, igra in ksp.igre.items() if igra.igralec == 0 and igra.racunalnik == 0]
-    for id_igre in igre_za_brisanje:
-        del ksp.igre[id_igre]
-    ksp.shrani_v_datoteko()
+@bottle.route('/ksp/', method=['GET','HEAD'])
+def igra_ksp():
+    if request.method == 'HEAD':
+        response.status = 200
+        return
+    else:
+        id_igre = bottle.request.get_cookie(ID_IGRE_COKOLADNI_PISKOT, secret=STARI_SLOVENSKI_PREGOVOR)
+        if id_igre is None:
+            response.status = 303
+            response.set_header("Location", "/nova_igra_ksp/")
+            return
+        else:
+            id_igre = int(id_igre)
+            if id_igre not in ksp.igre:
+                response.status = 303
+                response.set_header("Location", "/nova_igra_ksp/")
+                return
+        
+            # print(id_igre)
+            igra = ksp.igre[id_igre]
+            return bottle.template("views/ksp.tpl", igra=igra, id_igre=id_igre)
 
-    seznam_iger = []
-    for id_igre, igra in ksp.igre.items():
-        if igra.igralec > 0 or igra.racunalnik > 0:
-            opis = f"ID {id_igre}: igralec - {igra.igralec}, računalnik - {igra.racunalnik}"
-            seznam_iger.append(opis)
-    return bottle.template("views/zgodovina_ksp.tpl", igre=seznam_iger, uporabnik=uporabnik.upper())
 
-@bottle.delete('/brisi_ksp/')
+@bottle.route('/ksp/', method=['POST','HEAD'])
+def izbira_igralca_ksp():
+    if request.method == 'HEAD':
+        response.status = 303
+        response.set_header("Location", "/ksp/")
+        return
+    else:
+        id_igre = int(bottle.request.get_cookie(ID_IGRE_COKOLADNI_PISKOT, secret=STARI_SLOVENSKI_PREGOVOR))
+        orozje = int(bottle.request.forms["orozje"])
+        ksp.potek_igre(id_igre, orozje)
+        response.status = 303
+        response.set_header("Location", "/ksp/")
+        return
+
+@bottle.route('/zgodovina_ksp/', method=['GET','HEAD'])
+def prikazi_zgodovino():
+    if request.method == 'HEAD':
+        response.status = 200
+        return
+    else:
+        uporabnik = bottle.request.get_cookie("uporabnik", secret=STARI_SLOVENSKI_PREGOVOR)
+        if uporabnik is None:
+            response.status = 303
+            response.set_header("Location", "/")
+            return
+    
+        ksp.preberi_iz_datoteke()
+        igre_za_brisanje = [id_igre for id_igre, igra in ksp.igre.items() if igra.igralec == 0 and igra.racunalnik == 0]
+        for id_igre in igre_za_brisanje:
+            del ksp.igre[id_igre]
+        ksp.shrani_v_datoteko()
+
+        seznam_iger = []
+        for id_igre, igra in ksp.igre.items():
+            if igra.igralec > 0 or igra.racunalnik > 0:
+                opis = f"ID {id_igre}: igralec - {igra.igralec}, računalnik - {igra.racunalnik}"
+                seznam_iger.append(opis)
+        return bottle.template("views/zgodovina_ksp.tpl", igre=seznam_iger, uporabnik=uporabnik.upper())
+
+@bottle.route('/brisi_ksp/', method=['DELETE','HEAD'])
 def brisi_igre_kps():
-    ksp.igre.clear()
-    ksp.shrani_v_datoteko()
-    return bottle.redirect(f"/zgodovina_ksp/")
+    if request.method == 'HEAD':
+        response.status = 303
+        response.set_header("Location", "/zgodovina_ksp/")
+        return
+    else:
+        ksp.igre.clear()
+        ksp.shrani_v_datoteko()
+        response.status = 303
+        response.set_header("Location", "/zgodovina_ksp/")
+        return
 
-@bottle.get("/zgodovina_ksp/json/")
+@bottle.route('/zgodovina_ksp/json/', method=['GET','HEAD'])
 def zgodovina_json():
-    ksp.preberi_iz_datoteke()
-    igre = [
-        {"id": id_igre, "igralec": igra.igralec, "racunalnik": igra.racunalnik}
-        for id_igre, igra in ksp.igre.items()
-    ]
-    bottle.response.content_type = 'application/json'
-    return json.dumps(igre, indent=4)
-
-@bottle.get("/zgodovina_ksp/xml/")
+    if request.method == 'HEAD':
+        response.status = 200
+        return
+    else:
+        ksp.preberi_iz_datoteke()
+        igre = [
+            {"id": id_igre, "igralec": igra.igralec, "racunalnik": igra.racunalnik}
+            for id_igre, igra in ksp.igre.items()
+        ]
+        bottle.response.content_type = 'application/json'
+        return json.dumps(igre, indent=4)
+    
+@bottle.route('/zgodovina_ksp/xml/', method=['GET','HEAD'])
 def zgodovina_xml():
-    ksp.preberi_iz_datoteke()
-    root = ET.Element("igre")
-    for id_igre, igra in ksp.igre.items():
-        igra_el = ET.SubElement(root, "igra", id=str(id_igre))
-        ET.SubElement(igra_el, "igralec").text = str(igra.igralec)
-        ET.SubElement(igra_el, "racunalnik").text = str(igra.racunalnik)
+    if request.method == 'HEAD':
+        response.status = 200
+        return
+    else:
+        ksp.preberi_iz_datoteke()
+        root = ET.Element("igre")
+        for id_igre, igra in ksp.igre.items():
+            igra_el = ET.SubElement(root, "igra", id=str(id_igre))
+            ET.SubElement(igra_el, "igralec").text = str(igra.igralec)
+            ET.SubElement(igra_el, "racunalnik").text = str(igra.racunalnik)
 
-    bottle.response.content_type = 'application/xml'
-    return ET.tostring(root, encoding="unicode")
+        bottle.response.content_type = 'application/xml'
+        return ET.tostring(root, encoding="unicode")
 #====================================================================================================================================================
 
-@bottle.get("/nova_igra_kspov/")
+@bottle.route('/nova_igra_kspov/', method=['GET','HEAD'])
 def nova_igra_1():
-    id_nova_igra = kspov.nova_igra_1()
-    # print(id_nova_igra)
-    bottle.response.set_cookie(ID_IGRE_COKOLADNI_PISKOT, str(id_nova_igra), path='/', secret=STARI_SLOVENSKI_PREGOVOR)
-    return bottle.redirect(f'/kspov/')
-
-@bottle.get('/kspov/')
-def igra_ksp():
-    id_igre = bottle.request.get_cookie(ID_IGRE_COKOLADNI_PISKOT, secret=STARI_SLOVENSKI_PREGOVOR)
-    if id_igre is None:
-        return bottle.redirect(f"/nova_igra_kspov/")
+    if request.method == 'HEAD':
+        response.status = 303
+        response.set_header("Location", "/kspov/")
+        return
     else:
-        id_igre = int(id_igre)
-        if id_igre not in kspov.igre:
-            return bottle.redirect(f"/nova_igra_kspov/")
-        
-        # print(id_igre)
-        igra = kspov.igre[id_igre]
-        return bottle.template("views/kspov.tpl", igra=igra, id_igre=id_igre)
-
-@bottle.post('/kspov/')
-def izbira_igralca_kspov():
-    id_igre = int(bottle.request.get_cookie(ID_IGRE_COKOLADNI_PISKOT, secret=STARI_SLOVENSKI_PREGOVOR))
-    orozje = int(bottle.request.forms["orozje"])
-    kspov.potek_igre_1(id_igre, orozje)
-    return bottle.redirect(f"/kspov/")
-
-@bottle.get("/zgodovina_kspov/")
-def prikazi_zgodovino():
-    uporabnik = bottle.request.get_cookie("uporabnik", secret=STARI_SLOVENSKI_PREGOVOR)
-    if uporabnik is None:
-        return bottle.redirect(f"/")
+        id_nova_igra = kspov.nova_igra_1()
+        # print(id_nova_igra)
+        bottle.response.set_cookie(ID_IGRE_COKOLADNI_PISKOT, str(id_nova_igra), path='/', secret=STARI_SLOVENSKI_PREGOVOR)
+        response.status = 303
+        response.set_header("Location", "/kspov/")
+        return
     
-    kspov.preberi_iz_datoteke()
-    igre_za_brisanje = [id_igre for id_igre, igra in kspov.igre.items() if igra.igralec == 0 and igra.racunalnik == 0]
-    for id_igre in igre_za_brisanje:
-        del kspov.igre[id_igre]
-    kspov.shrani_v_datoteko()
+@bottle.route('/kspov/', method=['GET','HEAD'])
+def igra_ksp():
+    if request.method == 'HEAD':
+        response.status = 303
+        response.set_header("Location", "/kspov/")
+        return
+    else:
+        id_igre = bottle.request.get_cookie(ID_IGRE_COKOLADNI_PISKOT, secret=STARI_SLOVENSKI_PREGOVOR)
+        if id_igre is None:
+            response.status = 303
+            response.set_header("Location", "/nova_igra_kspov/")
+            return
+        else:
+            id_igre = int(id_igre)
+            if id_igre not in kspov.igre:
+                response.status = 303
+                response.set_header("Location", "/nova_igra_kspov/")
+                return
+        
+            # print(id_igre)
+            igra = kspov.igre[id_igre]
+            return bottle.template("views/kspov.tpl", igra=igra, id_igre=id_igre)
+        
+@bottle.route('/kspov/', method=['POST','HEAD'])
+def izbira_igralca_kspov():
+    if request.method == 'HEAD':
+        response.status = 303
+        response.set_header("Location", "/kspov/")
+        return
+    else:
+        id_igre = int(bottle.request.get_cookie(ID_IGRE_COKOLADNI_PISKOT, secret=STARI_SLOVENSKI_PREGOVOR))
+        orozje = int(bottle.request.forms["orozje"])
+        kspov.potek_igre_1(id_igre, orozje)
+        response.status = 303
+        response.set_header("Location", "/kspov/")
+        return
 
-    seznam_iger = []
-    for id_igre, igra in kspov.igre.items():
-        if igra.igralec > 0 or igra.racunalnik > 0:
-            opis = f"ID {id_igre}: igralec - {igra.igralec}, računalnik - {igra.racunalnik}"
-            seznam_iger.append(opis)
-    return bottle.template("views/zgodovina_kspov.tpl", igre=seznam_iger, uporabnik=uporabnik.upper())
+@bottle.route('/zgodovina_kspov/', method=['GET','HEAD'])
+def prikazi_zgodovino():
+    if request.method == 'HEAD':
+        response.status = 200
+        return
+    else:
+        uporabnik = bottle.request.get_cookie("uporabnik", secret=STARI_SLOVENSKI_PREGOVOR)
+        if uporabnik is None:
+            response.status = 303
+            response.set_header("Location", "/")
+            return
+    
+        kspov.preberi_iz_datoteke()
+        igre_za_brisanje = [id_igre for id_igre, igra in kspov.igre.items() if igra.igralec == 0 and igra.racunalnik == 0]
+        for id_igre in igre_za_brisanje:
+            del kspov.igre[id_igre]
+        kspov.shrani_v_datoteko()
 
-@bottle.delete('/brisi_kspov/')
-def brisi_igre_kps():
-    kspov.igre.clear()
-    kspov.shrani_v_datoteko()
-    return bottle.redirect(f"/zgodovina_kspov/")
+        seznam_iger = []
+        for id_igre, igra in kspov.igre.items():
+            if igra.igralec > 0 or igra.racunalnik > 0:
+                opis = f"ID {id_igre}: igralec - {igra.igralec}, računalnik - {igra.racunalnik}"
+                seznam_iger.append(opis)
+        return bottle.template("views/zgodovina_kspov.tpl", igre=seznam_iger, uporabnik=uporabnik.upper())
 
-@bottle.get("/zgodovina_kspov/json/")
+@bottle.route('/brisi_kspov/', method=['DELETE','HEAD'])
+def brisi_igre_kpsov():
+    if request.method == 'HEAD':
+        response.status = 303
+        response.set_header("Location", "/zgodovina_kspov/")
+        return
+    else:
+        kspov.igre.clear()
+        kspov.shrani_v_datoteko()
+        response.status = 303
+        response.set_header("Location", "/zgodovina_kspov/")
+        return
+
+@bottle.route('/zgodovina_kspov/json/', method=['GET','HEAD'])
 def zgodovina_json():
-    kspov.preberi_iz_datoteke()
-    igre = [
-        {"id": id_igre, "igralec": igra.igralec, "racunalnik": igra.racunalnik}
-        for id_igre, igra in kspov.igre.items()
-    ]
-    bottle.response.content_type = 'application/json'
-    return json.dumps(igre, indent=4)
-
-@bottle.get("/zgodovina_kspov/xml/")
+    if request.method == 'HEAD':
+        response.status = 200
+        return
+    else:
+        kspov.preberi_iz_datoteke()
+        igre = [
+            {"id": id_igre, "igralec": igra.igralec, "racunalnik": igra.racunalnik}
+            for id_igre, igra in kspov.igre.items()
+        ]
+        bottle.response.content_type = 'application/json'
+        return json.dumps(igre, indent=4)
+    
+@bottle.route('/zgodovina_kspov/xml/', method=['GET','HEAD'])
 def zgodovina_xml():
-    kspov.preberi_iz_datoteke()
-    root = ET.Element("igre")
-    for id_igre, igra in kspov.igre.items():
-        igra_el = ET.SubElement(root, "igra", id=str(id_igre))
-        ET.SubElement(igra_el, "igralec").text = str(igra.igralec)
-        ET.SubElement(igra_el, "racunalnik").text = str(igra.racunalnik)
+    if request.method == 'HEAD':
+        response.status = 200
+        return
+    else:
+        kspov.preberi_iz_datoteke()
+        root = ET.Element("igre")
+        for id_igre, igra in kspov.igre.items():
+            igra_el = ET.SubElement(root, "igra", id=str(id_igre))
+            ET.SubElement(igra_el, "igralec").text = str(igra.igralec)
+            ET.SubElement(igra_el, "racunalnik").text = str(igra.racunalnik)
 
-    bottle.response.content_type = 'application/xml'
-    return ET.tostring(root, encoding="unicode")
+        bottle.response.content_type = 'application/xml'
+        return ET.tostring(root, encoding="unicode")
 
 app = bottle.default_app()
 
-@app.hook('after_request')
-def advertise_http3():
-    response.set_header('Alt-Svc', 'h3=":4433"; ma=86400')
+# @app.hook('after_request')
+# def advertise_http3():
+    # xresponse.set_header('Alt-Svc', 'h3=":4433"; h3-29=":4433"; ma=86400')
 
 # @app.hook('before_request')
 # def enforce_https():
